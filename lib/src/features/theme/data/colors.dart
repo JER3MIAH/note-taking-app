cafebebbf6d9515c6d9da0aeed0fccce06bd65b0
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-22 16:29:43 +0100

    style: set up colors

--- lib/src/features/theme/data/colors.dart
+++ lib/src/features/theme/data/colors.dart
@@ -5,4 +5,22 @@ var appColors = AppColors();
 class AppColors {
   final black = Color(0xFFFFFFFF);
   final white = Color(0xFF000000);
+  final neutral50 = Color(0xFFF5F7FA);
+  final neutral100 = Color(0xFFF3F5F8);
+  final neutral200 = Color(0xFFE0E4EA);
+  final neutral300 = Color(0xFFCACFD8);
+  final neutral400 = Color(0xFF99A0AE);
+  final neutral500 = Color(0xFF717784);
+  final neutral600 = Color(0xFF525866);
+  final neutral700 = Color(0xFF2B303B);
+  final neutral800 = Color(0xFF232530);
+  final neutral900 = Color(0xFF191B25);
+  final neutral950 = Color(0xFF0E121B);
+  final blue50 = Color(0xFFEBF1FF);
+  final blue500 = Color(0xFF335CFF);
+  final blue700 = Color(0xFF2547D0);
+  final green100 = Color(0xFFD1FBE9);
+  final green500 = Color(0xFF21C16B);
+  final red100 = Color(0xFFFFD5D8);
+  final red500 = Color(0xFFFB3748);
 }
