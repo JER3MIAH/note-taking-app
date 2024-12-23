4edc9d739ebb2b428e5a25b7eae75960f4add45f
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-23 15:25:11 +0100

    test: update theme bloc unit test

--- test/src/features/settings/logic/blocs/theme_bloc_test.dart
+++ test/src/features/settings/logic/blocs/theme_bloc_test.dart
@@ -2,12 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:note_taking_app/src/features/settings/data/enums/enums.dart';
 import 'package:note_taking_app/src/features/settings/logic/blocs/theme_bloc/theme_state.dart';
+import 'package:note_taking_app/src/features/settings/logic/services/theme_local_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   late ThemeBloc themeBloc;
+  late SharedPreferences prefs;
 
-  setUp(() {
-    themeBloc = ThemeBloc();
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    prefs = await SharedPreferences.getInstance();
+    themeBloc = ThemeBloc(localService: ThemeLocalService(prefs: prefs));
   });
 
   tearDown(() {
@@ -22,6 +27,21 @@ void main() {
         final state = bloc.state;
         expect(state, ThemeState(appTheme: AppTheme.system));
       },
+    );
+
+    blocTest<ThemeBloc, ThemeState>(
+      'emits [AppTheme] or null when [GetSavedThemePrefs] is added',
+      build: () {
+        return themeBloc;
+      },
+      act: (bloc) => bloc.add(GetSavedThemePrefs()),
+      expect: () => [
+        isA<ThemeState>().having(
+          (state) => state.appTheme,
+          'App Theme',
+          isA<AppTheme?>(),
+        ),
+      ],
     );
 
     blocTest<ThemeBloc, ThemeState>(
