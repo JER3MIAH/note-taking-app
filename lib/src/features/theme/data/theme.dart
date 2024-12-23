28d733aeb972e786e8bcc23cf78e32215609d5c0
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-23 04:22:43 +0100

    style: set up theme

--- lib/src/features/theme/data/theme.dart
+++ lib/src/features/theme/data/theme.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:note_taking_app/src/features/theme/data/colors.dart';
 
 final lightTheme = ThemeData(
   useMaterial3: true,
+  colorScheme: ColorScheme.light(
+    primary: appColors.blue500,
+    secondary: appColors.blue50,
+    tertiary: appColors.blue700,
+  ),
 );
 
 final darkTheme = ThemeData(
   useMaterial3: true,
+  colorScheme: ColorScheme.dark(
+    primary: appColors.blue500,
+    secondary: appColors.blue50,
+    tertiary: appColors.blue700,
+  ),
 );
