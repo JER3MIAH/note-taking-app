b8a4c12c0069f9ee2351d02f37e759856d231db6
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-23 16:07:31 +0100

    chore: register font local service(get_it)

--- lib/src/features/settings/settings_injection_container.dart
+++ lib/src/features/settings/settings_injection_container.dart
@@ -4,12 +4,14 @@ import 'package:note_taking_app/src/features/settings/logic/blocs/theme_bloc/the
 import 'package:note_taking_app/src/features/settings/logic/services/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
 class SettingsInjectionContainer {
   static Future<void> init() async {
     //* Services
     getIt.registerLazySingleton<ThemeLocalService>(
       () => ThemeLocalService(prefs: getIt<SharedPreferences>()),
+    );
+    getIt.registerLazySingleton<FontLocalService>(
+      () => FontLocalService(prefs: getIt<SharedPreferences>()),
     );
 
     //* Blocs
