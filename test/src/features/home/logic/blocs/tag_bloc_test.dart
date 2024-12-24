f4c0475d766dccd5c4fb8ff30c8a3054cb88ddfd
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-24 15:25:28 +0100

    test: add tag bloc test

--- test/src/features/home/logic/blocs/tag_bloc_test.dart
+++ test/src/features/home/logic/blocs/tag_bloc_test.dart
@@ -1,5 +1,8 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:note_taking_app/src/features/home/logic/blocs/tag_bloc/tag_bloc.dart';
+import 'package:note_taking_app/src/features/home/logic/blocs/tag_bloc/tag_event.dart';
+import 'package:note_taking_app/src/features/home/logic/blocs/tag_bloc/tag_state.dart';
 import 'package:note_taking_app/src/features/home/logic/services/tag_local_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,5 +20,87 @@ void main() {
     tagBloc.close();
   });
 
-  group('Tag Bloc -', () {});
+  group('Tag Bloc -', () {
+    final testTag = 'TestTag';
+    final testTag2 = 'TestTag2';
+
+    blocTest<TagBloc, TagState>(
+      'emits [TagState] with an empty tag list when [GetTags] is added.',
+      build: () => tagBloc,
+      act: (bloc) => bloc.add(GetTags()),
+      expect: () => [
+        isA<TagState>().having(
+          (state) => state.tags,
+          'tags',
+          [],
+        ),
+      ],
+    );
+
+    blocTest<TagBloc, TagState>(
+      'emits [TagState] with a null selected tag when [GetSelectedTag] is added.',
+      build: () => tagBloc,
+      act: (bloc) => bloc.add(GetSelectedTag()),
+      expect: () => [
+        isA<TagState>().having(
+          (state) => state.selectedTag,
+          'selected tag',
+          null,
+        ),
+      ],
+    );
+
+    blocTest<TagBloc, TagState>(
+      'emits [TagState] with updated selected tag when [SelectTag] is added.',
+      build: () => tagBloc,
+      act: (bloc) => bloc.add(SelectTag(tag: testTag)),
+      expect: () => [
+        isA<TagState>().having(
+          (state) => state.selectedTag,
+          'selected tag',
+          testTag,
+        ),
+      ],
+    );
+
+    blocTest<TagBloc, TagState>(
+      'emits [TagState] with null selected tag when [ResetSelectedTag] is added.',
+      build: () => tagBloc,
+      act: (bloc) => bloc.add(ResetSelectedTag()),
+      expect: () => [
+        isA<TagState>().having(
+          (state) => state.selectedTag,
+          'selected tag',
+          null,
+        ),
+      ],
+    );
+
+    blocTest<TagBloc, TagState>(
+      'emits [TagState] with null selected tag when [AddTags] is added.',
+      build: () => tagBloc,
+      act: (bloc) => bloc.add(AddTags(tags: [testTag, testTag2])),
+      expect: () => [
+        isA<TagState>().having(
+          (state) => state.tags,
+          'tags',
+          [testTag, testTag2],
+        ),
+      ],
+    );
+
+    blocTest<TagBloc, TagState>(
+      'emits [TagState] with provided tags deleted from tag list when [DeleteTags] is added.',
+      build: () => tagBloc,
+      seed: () => TagState(tags: [testTag, testTag2]),
+      act: (bloc) => bloc.add(DeleteTags(tags: [testTag])),
+      expect: () => [
+        isA<TagState>().having(
+          (state) => state.tags,
+          'tags',
+          [testTag2],
+        ),
+      ],
+    );
+  });
 }
