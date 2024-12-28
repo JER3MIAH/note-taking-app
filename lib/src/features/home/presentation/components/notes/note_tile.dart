dc682ed874e0426b161817d8eea247f3fecfcbcd
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-28 11:09:16 +0100

    feat: implement note tile

--- lib/src/features/home/presentation/components/notes/note_tile.dart
+++ lib/src/features/home/presentation/components/notes/note_tile.dart
@@ -5,69 +5,77 @@ import 'package:note_taking_app/src/shared/shared.dart';
 class NoteTile extends StatelessWidget {
   final Note note;
   final bool isSelected;
+  final VoidCallback? onTap;
   const NoteTile({
     super.key,
     required this.note,
     this.isSelected = false,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
 
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.all(spacing100),
-      decoration: BoxDecoration(
-        borderRadius: isSelected ? BorderRadius.circular(spacing75) : null,
-        border: !isSelected
-            ? Border(
-                bottom: BorderSide(
-                  color: theme.inversePrimary,
+    return GestureDetector(
+      onTap: onTap,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(spacing100),
+          decoration: BoxDecoration(
+            borderRadius: isSelected ? BorderRadius.circular(spacing75) : null,
+            border: !isSelected
+                ? Border(
+                    bottom: BorderSide(
+                      color: theme.inversePrimary,
+                    ),
+                  )
+                : null,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppText(
+                note.title,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+              YBox(10),
+              Wrap(
+                spacing: 4,
+                runSpacing: 4,
+                children: List.generate(
+                  note.tags.length,
+                  (index) {
+                    final tag = note.tags[index];
+                    return Container(
+                      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                      decoration: BoxDecoration(
+                        color: theme.inversePrimary,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: AppText(
+                        tag,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    );
+                  },
                 ),
-              )
-            : null,
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AppText(
-            note.title,
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
+              ),
+              YBox(10),
+              AppText(
+                formatDate(note.createdAt, format: 'dd MMM yyyy'),
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: theme.surfaceBright,
+              ),
+            ],
           ),
-          YBox(10),
-          Wrap(
-            spacing: 4,
-            runSpacing: 4,
-            children: List.generate(
-              note.tags.length,
-              (index) {
-                final tag = note.tags[index];
-                return Container(
-                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                  decoration: BoxDecoration(
-                    color: theme.inversePrimary,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: AppText(
-                    tag,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                  ),
-                );
-              },
-            ),
-          ),
-          YBox(10),
-          AppText(
-            formatDate(note.createdAt, format: 'dd MMM yyyy'),
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            color: theme.surfaceBright,
-          ),
-        ],
+        ),
       ),
     );
   }
